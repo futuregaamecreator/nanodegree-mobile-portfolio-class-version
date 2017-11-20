042c735fcdce298ab655ded0bdac816efd9bbd41
George Harrison <futuregamecreator@gmail.com>
2017-11-20 12:47:46 -0500

    update

--- views/js/main.js
+++ views/js/main.js
@@ -501,40 +501,47 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
+//phases to make loop easier
+  var phases = [];
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop / 1250;
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    // document.body.scrollTop is no longer supported in Chrome.
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  for (var i = 0; i < 5; i++) {
+    phases.push(100 * Math.sin(scrollTop + i));
+    }
+    // set phases
+    for (i = 0; i < items.length; i++) {
+      items[i].style.left = items[i].basicLeft + phases[i % 5] + 'px';
+    }
+
+    // User Timing API to the rescue again. Seriously, it's worth learning.
+    // Super easy to create custom metrics.
+    window.performance.mark("mark_end_frame");
+    window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
+    if (frame % 10 === 0) {
+      var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
+      logAverageFrame(timesToUpdatePosition);
+    }
   }
 
-  // User Timing API to the rescue again. Seriously, it's worth learning.
-  // Super easy to create custom metrics.
-  window.performance.mark("mark_end_frame");
-  window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
-  if (frame % 10 === 0) {
-    var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
-    logAverageFrame(timesToUpdatePosition);
-  }
-}
+  // runs updatePositions on scroll with requestAnimationFrame
+  window.addEventListener('scroll', function() {
+    window.requestAnimationFrame(updatePositions);
+  });
 
-// runs updatePositions on scroll
-window.addEventListener('scroll', updatePositions);
-
-// Generates the sliding pizzas when the page loads.
-document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
-  var s = 256;
-  for (var i = 0; i < 40; i++) {
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
-    elem.basicLeft = (i % cols) * s;
-    elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
-  }
-  updatePositions();
-});
+  // Generates the sliding pizzas when the page loads.
+  document.addEventListener('DOMContentLoaded', function() {
+    var cols = 8;
+    var s = 256;
+    var movingPizzas1 = document.getElementById("movingPizzas1");
+    for (var i = 0; i < 32; i++) {
+      var elem = document.createElement('img');
+      elem.className = 'mover';
+      elem.src = "images/pizza.png";
+      elem.style.height = "100px";
+      elem.style.width = "73.333px";
+      elem.basicLeft = (i % cols) * s;
+      elem.style.top = (Math.floor(i / cols) * s) + 'px';
+      movingPizzas1.appendChild(elem);
+    }
+    updatePositions();
+  });
